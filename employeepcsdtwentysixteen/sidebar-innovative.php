67553070199581144219828a322ca36a3f8d36b4
Espi <joshespi@gmail.com>
2022-08-02 15:43:15 -0600

    made a few more responsive styling adjustments.  reorganized the employee front page stylesheet so its also in document order.

--- employeepcsdtwentysixteen/sidebar-innovative.php
+++ employeepcsdtwentysixteen/sidebar-innovative.php
@@ -1,12 +1,11 @@
 <aside id="sidebar" class="sidebar">
 	<ul class="imagelist">
-		<l<li>
+		<li>
 			<a href="https://provo.edu/category/news/provo-way-learning-initiative/">
 				<img src="//globalassets.provo.edu/image/icons/policies-lt.svg" alt="" />
 				<span>Learning Initiative News</span>
 			</a>
 		</li>
-		
 	</ul>
 	<h2>Provo Way Innovative Learning Initiative Contacts</h2>
 		<?php  
