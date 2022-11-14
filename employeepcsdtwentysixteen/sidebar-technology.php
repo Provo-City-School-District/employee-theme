1d6759dafb13231fd2e0e79444b2ed9cb2e3b540
Espi <joshespi@gmail.com>
2022-11-14 13:18:27 -0700

    changes not made by joshe

--- employeepcsdtwentysixteen/sidebar-technology.php
+++ employeepcsdtwentysixteen/sidebar-technology.php
@@ -1,4 +1,5 @@
 <aside id="sidebar" class="sidebar technology">
+<a href="https://helpdesk.provo.edu/helpdesk/WebObjects/Helpdesk.woa" class="bigRedButton">Submit a Work Order</a>
 	<ul class="imagelist">
 		<li>
 			<a href="https://employee.provo.edu/category/employee-news/technology-support/">
@@ -6,19 +7,32 @@
 				<span>Technology Support News</span>
 			</a>
 		</li>
-		<li>
-			<a href="https://sos.splashtop.com/">
-				<img src="//globalassets.provo.edu/image/icons/tech-2-lt.svg" alt="" />
-				<span>Open a Remote Session</span>
-			</a>
-		</li>
 	</ul>
-	<h2>Technology Contacts</h2>
-		<?php 
-			
-			echo file_get_contents('https://provo.edu/directory_page/tech-support-admin-sidebar/');
-			echo file_get_contents('https://provo.edu/directory_page/tech-support-staff-sidebar/');	
-			echo '<h3>Field Technicians</h3>';
-			echo file_get_contents('https://provo.edu/directory_page/technology-field-tech/');	
+	<h2>How to Backup Your Computer</h2>
+	<ul class="blt">
+		<li><a href="https://www.lifewire.com/ways-to-back-up-your-data-2640426">5 Ways to Back up Your Data - Lifewire</a></li>
+		
+		<li><a href="https://www.wikihow.com/Back-Up-a-Mac">How to Back Up a Mac - WikiHow</a></li>
+		
+		<li><a href="https://support.apple.com/mac-backup">How to Backup Your Mac - Apple</a></li>
+		
+		<li><a href="https://www.wikihow.com/Back-up-a-Computer">How to Back up a Computer - WikiHow</a></li>
+	</ul>
+	
+	<h2>How to Care for Your Device</h2>
+	
+	<ul class="blt">
+	<li>Shut down the computer, disconnect the power adapter and any peripherals.</li>
+	<li>Use a damp, soft, lint-free cloth to clean the computer’s exterior. Avoid getting moisture in any openings. NEVER spray liquid directly on the computer.</li>
+	<li>Don’t use aerosol sprays, solvents, or abrasives.</li> 
+	<li>MAGIC ERASERS are actually somewhat abrasive so just barely dampen them and gently wipe, do not rub, across the keyboard and wrist rest. </li>
+	<li>Wipe gently with a damp Clorox wipe and then follow with a soft, lint-free cloth to dry off any surface after cleaning it.</li>
+	</ul>
+	<!-- <h2>Technology Contacts</h2> -->
+		<?php 	
+		//	echo file_get_contents('https://provo.edu/directory_page/tech-support-admin-sidebar/');
+		//	echo file_get_contents('https://provo.edu/directory_page/tech-support-staff-sidebar/');	
+		//	echo '<h3>Field Technicians</h3>';
+		//  echo file_get_contents('https://provo.edu/directory_page/technology-field-tech/');	
 		?>
 </aside>
