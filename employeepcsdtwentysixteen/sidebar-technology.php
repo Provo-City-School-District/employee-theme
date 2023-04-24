4a681d794ae82c848b952a093ac0100e388cb15e
Espi <joshespi@gmail.com>
2023-04-24 09:54:03 -0600

    rollup

--- employeepcsdtwentysixteen/sidebar-technology.php
+++ employeepcsdtwentysixteen/sidebar-technology.php
@@ -8,25 +8,23 @@
 			</a>
 		</li>
 	</ul>
-	<h2>How to Backup Your Computer</h2>
+	<h2>Forms &amp; Documents</h2>
 	<ul class="blt">
-		<li><a href="https://www.lifewire.com/ways-to-back-up-your-data-2640426">5 Ways to Back up Your Data - Lifewire</a></li>
-		
-		<li><a href="https://www.wikihow.com/Back-Up-a-Mac">How to Back Up a Mac - WikiHow</a></li>
-		
-		<li><a href="https://support.apple.com/mac-backup">How to Backup Your Mac - Apple</a></li>
-		
-		<li><a href="https://www.wikihow.com/Back-up-a-Computer">How to Back up a Computer - WikiHow</a></li>
+		<li><a href="https://docs.google.com/spreadsheets/d/1igjb01OQzXyl6C41GKPg3dsu3ipdrt4qQoau_ZXhIRg/edit#gid=0">Approved Computer Hardware</a></li>
+		<li><a href="https://employee.provo.edu/wp-content/uploads/2017/02/11072017-Hardware-Request-Form.pdf">Request for Hardware Approval</a></li>
 	</ul>
-	
-	<h2>How to Care for Your Device</h2>
-	
+	<h2>Disaster Recovery Procedures</h2>
 	<ul class="blt">
-	<li>Shut down the computer, disconnect the power adapter and any peripherals.</li>
-	<li>Use a damp, soft, lint-free cloth to clean the computer’s exterior. Avoid getting moisture in any openings. NEVER spray liquid directly on the computer.</li>
-	<li>Don’t use aerosol sprays, solvents, or abrasives.</li> 
-	<li>MAGIC ERASERS are actually somewhat abrasive so just barely dampen them and gently wipe, do not rub, across the keyboard and wrist rest. </li>
-	<li>Wipe gently with a damp Clorox wipe and then follow with a soft, lint-free cloth to dry off any surface after cleaning it.</li>
+		<li><a href="https://employee.provo.edu/wp-content/uploads/2018/08/08212018-Distaster-Recovery-Procedures-for-All-Users.pdf">Procedures for All Users</a></li>
+		<li><a href="https://employee.provo.edu/wp-content/uploads/2018/08/08212018-Distaster-Recovery-Procedures-for-Increased-Risk-Users.pdf">Procedures for Increased Risk Users</a></li>
+		<li><a href="https://employee.provo.edu/wp-content/uploads/2018/08/08212018-Distaster-Recovery-Procedures-for-Technology-Department.pdf">Procedures for Technology Department</a></li>
+	</ul>
+	<h2>Student Data Privacy</h2>
+	<p>These are links and documents related to state law concerning student data privacy.</p>
+	<ul class="blt">
+		<li><a href="https://studentprivacycompass.org/resource/utah-state-board-of-education-data-dictionary/">Utah State Board of Education Metadata Dictionary</a></li>
+		<li><a href="https://provo.edu/our-district/student-data-privacy/">Student Data Privacy (Our District)</a></li>
+		<li><a href="https://employee.provo.edu/technology/approved-applications/">Approved Application List</a></li>
 	</ul>
 	<!-- <h2>Technology Contacts</h2> -->
 		<?php 	
