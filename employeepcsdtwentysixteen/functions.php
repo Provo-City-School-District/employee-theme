61991324999bd684276ec53d39d95604ab908907
Espi <joshespi@gmail.com>
2023-05-05 08:30:14 -0600

    fixed 404 page markup and styling
    ticket=105790

--- employeepcsdtwentysixteen/functions.php
+++ employeepcsdtwentysixteen/functions.php
@@ -13,7 +13,7 @@ function pcsd_scripts_styles() {
 	/*   REGISTER ALL CSS FOR SITE */
 	/*   CALL ALL CSS AND SCRIPTS FOR SITE */
 	wp_enqueue_style('variables', get_template_directory_uri() . '/assets/css/variables.css', '', '1.0.01', false);
-	wp_enqueue_style( 'parent_style', get_template_directory_uri().'/assets/css/employee-parent-styles.css', '', '1.00.05', false);
+	wp_enqueue_style( 'parent_style', get_template_directory_uri().'/assets/css/employee-parent-styles.css', '', '1.00.07', false);
 	wp_enqueue_style( 'style', get_stylesheet_uri(), array('parent_style'), '1.00.02', false);
 	wp_enqueue_style('header', get_template_directory_uri() . '/assets/css/header.css', '', '1.0.02', false);
 	wp_enqueue_script( 'theme_scripts',get_template_directory_uri() .'/assets/js/scripts.js', array() , '1.00.01', true);
