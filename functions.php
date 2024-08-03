9ffe29e9331f753f6195bf1060a71c70d62de108
Josh Espinoza <joshespi@gmail.com>
2024-08-03 04:28:42 -0600

    add updated shortcode for directory pull

--- functions.php
+++ functions.php
@@ -148,18 +148,30 @@ add_filter( 'manage_edit-post_sortable_columns', 'modified_column_register_sorta
 add_filter( 'manage_edit-page_sortable_columns', 'modified_column_register_sortable' );
 
 //[directory url=""]
+function directory_func($atts)
+{
+    $category = shortcode_atts(array(
+        'url' => 'something',
+    ), $atts);
+    $directory_url = "{$category['url']}";
 
-function directory_func($atts) {
-	$category = shortcode_atts( array(
-		'url' => 'something',
-		//'bar' => 'something else',
-	), $atts );
-	$directory_url = "{$category['url']}";
-	// echo '<div class="directoryGrid">';
-	return '<div class="directoryGrid">' . file_get_contents($directory_url) . '</div>';
-	// echo '</div>';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $directory_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // TODO: to verify certificate, but path to cerificate may move or change in the future. want to think through something so this doesn't get disjointed or forgotten, going to not verify for now
+    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    // curl_setopt($ch, CURLOPT_CAINFO, '/etc/ssl/wildcard/star_provo_edu.crt'); // Path to CA certificates bundle
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $output = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+    $output = '<div class="staff-member-listing">' . $output . '</div>';
+    return $output;
 }
-add_shortcode( 'directory', 'directory_func' );
+add_shortcode('directory', 'directory_func');
 
 /*-------------------------------------------------------*/
 /* Add Length Column to the Wordpress Dashboard
